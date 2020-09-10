c91a3060310bc9dd1d2b99a87fcb412ac9ef56fb
jiahuanluo <luojiahuan001@gmail.com>
2020-09-10 14:17:59 +0800

    dp

--- dp_utils.py
+++ dp_utils.py
@@ -61,3 +61,27 @@ def add_dp_v3(input_tensor, clip_value=5.0, variance=1.0):
         input_noised = torch.div(input_copy, norm_factor) + torch.normal(0, variance, input_tensor.shape).cuda()
     input_with_dp = torch.autograd.Variable(input_noised, requires_grad=True).cuda()
     return input_with_dp
+
+def add_dp_to_list(input_tensor_list, clip_value=5.0, variance=1.0):
+    """
+    gpu compatible version
+    :param input_tensor: variable
+    :param clip_value: clipping value of 2-norm
+    :param variance: variance of gaussian noise
+    :param device:
+    :return: variable with dp applied
+    """
+    output_list = []
+    for input_tensor in input_tensor_list:
+        if isinstance(input_tensor, tuple):
+            input_copy = input_tensor[0].detach().clone()
+        else:
+            input_copy = input_tensor.detach().clone()
+        with torch.no_grad():
+            # clip 2-norm per sample
+            norm_factor = torch.div(torch.max(torch.norm(input_copy, dim=1)), clip_value+1e-6).clamp(min=1.0)
+            # add gaussian noise
+            input_noised = torch.div(input_copy, norm_factor) + torch.normal(0, variance, input_copy.shape).cuda()
+        input_with_dp = torch.autograd.Variable(input_noised, requires_grad=True).cuda()
+        output_list.append(input_with_dp)
+    return output_list
